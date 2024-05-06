f93c5fbbec9f2d5a59ba971f6bfb676bdf2b00f9
Jeyker-Dev <mendozajeyker1178@gmail.com>
2024-05-06 09:59:09 -0400

    Agregando Archivos para el Proyecto

---
+++ includes/funciones.php
@@ -0,0 +1,14 @@
+<?php
+
+function showValues($variable) : string {
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
+
+// Escapa / Sanitizar el HTML
+function s($html) : string {
+    $s = htmlspecialchars($html);
+    return $s;
+}
